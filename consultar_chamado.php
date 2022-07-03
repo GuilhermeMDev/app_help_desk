07d05f59367cf9e15e1199e2d244260c8c436f67
Guilherme M <guimagioni@gmail.com>
2022-07-03 20:55:20 -0300

    consultando chamadas

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,6 +1,19 @@
+<?php require_once("validador_acesso.php"); ?>
+
 <?php
-require_once("validador_acesso.php");
+//chamados
+$chamados = [];
+
+
+$arquivo = fopen('arquivo.hd', 'r');//somente leitura
+while (!feof($arquivo)) { //funcao nativa do php, le cada linha do arquivo, retorna true ou false encontrando ou nao o final do arquivo EOL.
+    $registro = fgets($arquivo); //funcao nativa do php, recuperando o valor da linha onde esta o cursor do php dentro do arquivo.hd
+    $chamados[] = $registro;
+}
+fclose($arquivo); //fechando o arquivo aberto
+
 ?>
+
 
 <html>
 <head>
@@ -38,25 +51,29 @@ require_once("validador_acesso.php");
                 </div>
 
                 <div class="card-body">
+                    <!--  aqui estamos chamando a lógica para imprimir as chamadas criadas, armazenadas no arquivo.txt-->
+                    <?php foreach ($chamados as $chamado) { ?>
 
-                    <div class="card mb-3 bg-light">
-                        <div class="card-body">
-                            <h5 class="card-title">Título do chamado...</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                            <p class="card-text">Descrição do chamado...</p>
+                        <?php
+                        $chamado_dados = explode('#', $chamado);
 
+                        //tratando os erros ao imprimir o ultimo array vazio, devido a logica de iteracao do php na logica de
+                        // quebra de linha do PHP_EOL(END OF LINE)
+                        if (count($chamado_dados) < 3) {
+                            continue;
+                        }
+                        ?>
+
+                        <div class="card mb-3 bg-light">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $chamado_dados[0] ?></h5>
+                                <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1] ?></h6>
+                                <p class="card-text"><?= $chamado_dados[2] ?></p>
+
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="card mb-3 bg-light">
-                        <div class="card-body">
-                            <h5 class="card-title">Título do chamado...</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                            <p class="card-text">Descrição do chamado...</p>
-
-                        </div>
-                    </div>
-
+                    <?php } ?>
                     <div class="row mt-5">
                         <div class="col-6">
                             <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
